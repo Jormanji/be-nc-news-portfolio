2e37ce26c0d86a5a5093819738891b14aef80088
Jormanji <alacritydrummer@hotmail.co.uk>
2024-01-17 11:06:48 +0000

    Add all relevant functions for task 5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const {getTopics, getApiEndpoints, getArticleById} = require("./controllers/get.controllers")
+const {getTopics, getApiEndpoints, getArticleById, getArticles} = require("./controllers/get.controllers")
 
 app.use(express.json())
 
@@ -11,26 +11,37 @@ app.get("/api", getApiEndpoints)
 
 app.get("/api/articles/:article_id", getArticleById)
 
+app.get("/api/articles", getArticles)
 
-
-app.use(( req, res) => {
-  res.status(404).send({ message: "Not found"})
+app.all("*", (req, res) => {
+  res.status(404).send({message : "Not found"})
 })
 
-app.use((err, req, res, next) => {
-    if (err.code === "22P02"){
-      res.status(400).send({message : "Bad request"});
-    } else {
-      next(err)
-    }
-  })
-  app.use((err, req, res, next) =>{
-    if (err.message === "Bad request"){
-      res.status(400).send({message: err.message})
-    } else {
-        next(err)
-    }
-  })
 
+app.use((err, req, res, next) => {
+  if (err.status && err.message) {
+    res.status(err.status).send({ message: err.message });
+  } else {
+    next(err);
+  }
+});
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ message: "Bad request" });
+  } else {
+    next(err);
+  }
+});
+app.use((err, req, res, next) => {
+  if (err.message === "Bad request") {
+    res.status(400).send({ message: err.message });
+  } else {
+    next(err);
+  }
+});
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.status(500).send({message: 'internal server error'})
+})
 
 module.exports = app
--- models/get.models.js
+++ models/get.models.js
@@ -1,5 +1,6 @@
 const db = require("../db/connection")
 const fs = require("fs/promises")
+const { fetchCommentCount } = require("../db/seeds/utils")
 
 exports.fetchTopics = () => {
     return db.query("SELECT * FROM topics").then((result) => {
@@ -18,8 +19,28 @@ exports.fetchArticleById = (articleId) => {
     return db.query('SELECT * FROM articles WHERE article_id = $1', [articleId])
     .then((result) => {
         if(result.rows.length === 0){
-           return Promise.reject("Not found")
+           return Promise.reject({status: 404, message: "Not found"})
         } else {
         return result.rows[0]}
+    })
+}
+
+exports.fetchArticles = () => {
+    return db.query(`
+    SELECT 
+    articles.author, 
+    articles.title,
+    articles.article_id, 
+    articles.topic, 
+    articles.created_at, 
+    articles.votes, 
+    articles.article_img_url, 
+    COUNT(comments.comment_id)::INT AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    GROUP BY articles.article_id
+    ORDER BY created_at DESC`
+    ).then((result) => {
+        return result.rows
     })
 }
